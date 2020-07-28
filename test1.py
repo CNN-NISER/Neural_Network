c06351148aa44d9c3cce2ba5ea6f97219dbdb1c6
chinmay-routray <31167301+chinmay-routray@users.noreply.github.com>
2020-07-28 14:01:37 +0530

    Update test1.py
    Added test data

--- test1.py
+++ test1.py
@@ -25,9 +25,25 @@ train_Y.reshape(size,1)
 # x is the input and y is the desired output.
 train_data = list(zip(train_X,train_Y))
 
+# Testing Data
+low_test = 1
+high_test = 50000
+size_test = 10000
+
+inp_num_t = np.random.randint(low_test,high_test,size_test) #creates an array of the nos for training
+test_X = np.array(inp_num_t)
+test_X.reshape(size_test,1)
+
+test_Y = np.array(inp_num_t*3)
+test_Y.reshape(size_test,1)
+
+# The training data should be a list of tuples (x,y) where x and y are both 1-d arrays
+# x is the input and y is the desired output.
+test_data = list(zip(test_X,test_Y))
+
 
 # Training the model
 n_epoch = 30
 learning_rate = 3
 mini_batch_size = 100
-model.GradDesc(train_data, n_epoch, mini_batch_size, learning_rate, test_data=None)
+model.GradDesc(train_data, n_epoch, mini_batch_size, learning_rate, test_data=test_data)
